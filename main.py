08ecb088f387b9594d8e88b9cf03527e9c3863bd
Dubyk_Yura <dubykjurij@gmail.com>
2023-05-21 16:53:35 +0300

    Create abstract class and 3 class with different realization it also add manager

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-from Bicycle import Bicycle
+import sys
+from managers.bicycle_manager import BicycleManager
+from models.bicycle import Bicycle
+from models.electro_bicycle import ElectroBicycle
+from models.tricycle import Tricycle
+from models.unicycle import Unicycle
 
 if __name__ == "__main__":
-    bicycles = [Bicycle() for _ in range(4)]
-    bicycles[0] = Bicycle()
-    bicycles[1] = Bicycle("Some type", "Some brand ", 10, 20)
-    bicycles[2] = Bicycle.get_instance()
-    bicycles[3] = Bicycle.get_instance()
+    bicycles_manager = BicycleManager()
+    bicycles_manager.add_bicycle(Bicycle("Apple", "mountain", 4, 7))
+    bicycles_manager.add_bicycle(Bicycle("Xiaomi", "non-mountain", 30, 40))
+    bicycles_manager.add_bicycle(Unicycle(3, 2, "Uni-company", 2, 5))
+    bicycles_manager.add_bicycle(Unicycle(1, 2, "Evil Company", 1, 4))
+    bicycles_manager.add_bicycle(Tricycle(True, 2, "Tri-company", 5, 10))
+    bicycles_manager.add_bicycle(Tricycle(False, 1, "Company", 4, 6))
+    bicycles_manager.add_bicycle(ElectroBicycle(2, 10, "Samsung", 32, 45))
+    bicycles_manager.add_bicycle(ElectroBicycle(4, 15, "Electro", 30, 35))
 
-    for bicycle in bicycles:
-        print(f"Bicycle : {bicycle.__str__()}")
+    print("Print all bicycles ->")
+    for bicycle in bicycles_manager.bicycles:
+        print(bicycle)
+    print("\n")
 
-    elements = bicycles[1:3]
-    for bicycle2 in elements:
-        print(bicycle2)
+    print("Print with some conditions ->\n")
+    for bicycle in bicycles_manager.find_all_with_max_speed_higher_than(20):
+        print(bicycle)
+    print()
+
+    for bicycle in bicycles_manager.find_all_with_current_speed_lower_than(2):
+        print(bicycle)

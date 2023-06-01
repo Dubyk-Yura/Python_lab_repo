38f9d1fbed82293ced89615633c4768ec4b8b754
Dubyk_Yura <dubykjurij@gmail.com>
2023-06-01 22:03:40 +0300

    delete excess decorator

--- decorators.py
+++ decorators.py
@@ -1,30 +1,7 @@
 """
 This module provides custom decorators for enhancing the functionality of functions.
 """
-import functools
 import time
-
-
-def return_name(func):
-    """
-    Decorator that prints the name of the function before calling it.
-    Wrapper function that prints the function name and calls the original function.
-
-    Args:
-        func (function): The function to be decorated.
-
-    Returns:
-        function: The decorated function.
-    """
-
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        print(*args)
-        print(**kwargs)
-        print(f"Name of function -> {func.__name__}")
-        return func(*args, **kwargs)
-
-    return wrapper
 
 
 def from_iter_to_tuple(func):
